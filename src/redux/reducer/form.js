208672c0d54c4ee900f5e2fac212ece51b3b7972
Anoop Varghese <anupvarghese@gmail.com>
2017-06-05 12:46:52 +1000

    modify reducer

--- src/redux/reducer/form.js
+++ src/redux/reducer/form.js
@@ -3,11 +3,17 @@ import { fromJS } from 'immutable';
 export default (state = fromJS({}), action) => {
   switch (action.type) {
     case 'SET_FORM_NAME': {
-      const formName = action.data.name;
+      const formName = action.data;
       return state.merge({
         [formName]: {},
       });
     }
+
+    case 'UPDATE_INPUT_VALUE': {
+      const { formName, value, id } = action.data;
+      return state.setIn([formName, id], value);
+    }
+
     default: {
       return state;
     }
