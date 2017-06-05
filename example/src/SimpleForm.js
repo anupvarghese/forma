a6e9ca13acbd634edd383a9665f25fe0cff404a1
Anoop Varghese <anupvarghese@gmail.com>
2017-06-05 12:46:14 +1000

    name for children

--- example/src/SimpleForm.js
+++ example/src/SimpleForm.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Form } from '../../lib';
 
 class SimpleForm extends Component {
+
   render() {
     return (
-      <form>
-        <input type="text" id="123" />
-        <input type="text" id="124" />
-        <input type="text" id="125" />
+      <form >
+        <input type="text" id="123" name="aaaa" />
+        <input type="text" id="124" name="bbbb" />
+        <input type="text" id="125" name="cccc" />
       </form>
     );
   }
